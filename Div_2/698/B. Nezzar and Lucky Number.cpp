c16ec9befc711a6d93a65d7bba750e595b35e9d7
Mycodeiskuina <virginia.puente@utec.edu.pe>
2021-02-04 10:01:13 -0500

    698

---
+++ Div_2/698/B. Nezzar and Lucky Number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+const int N = 1e3 + 1;
+int n;
+vector<pii> rect[N];
+ 
+ 
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+ 
+	cin >> n;
+ 	cout << '\n';
+ 	return 0;
+}
