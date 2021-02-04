270a07110c57aec327d4b7f2bacd72058cb67904
Mycodeiskuina <virginia.puente@utec.edu.pe>
2021-02-04 10:16:51 -0500

    698_A

--- Div_2/698/A. Nezzar and Colorful Balls.cpp
+++ Div_2/698/A. Nezzar and Colorful Balls.cpp
@@ -1,16 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
-const int N = 1e3 + 1;
-int n;
-vector<pii> rect[N];
- 
+using ii = pair<int, int>;
+const int N = 1e2+10;
+int cont[N];
  
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
- 
-	cin >> n;
- 	cout << '\n';
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int mx=0;
+		for(int i=0;i<n;i++) {
+			int x;
+			cin>>x;
+			cont[x]++;
+			mx=max(mx,cont[x]);
+		}
+		for(int i=0;i<=n;i++) cont[i]=0;
+		cout<<mx<<'\n';
+	}
  	return 0;
 }
